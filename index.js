bd6a4b15a927da76ab7fc667c61e46272307f3cb
Samuel <sam3bett@gmail.com>
2025-06-26 16:06:38 +0300

    added return

--- index.js
+++ index.js
@@ -1,40 +1,20 @@
-
-
-
-
-
-// Automatically run fetchBooks when the page loads
-document.addEventListener('DOMContentLoaded', () => {
-  fetchBooks();
- 
-}
-);
-
-
-//fetching game of thrones books and passing them to the API
-
-
 function fetchBooks() {
-  fetch('https://anapioficeandfire.com/api/books')
-    .then(response => response.json())
-    .then(data => renderBooks(data))
+   return fetch('https://anapioficeandfire.com/api/books')
+    .then(res=> res.json())
+    .then(data => renderBooks(data));
 }
 
-//renderBooks function to display the books on the page
-document.addEventListener('DOMContentLoaded', () => {
-  fetchBooks();
-});
 function renderBooks(books) {
-  const main = document.querySelector('main');
+  const main = document.querySelector("main");
+  // bookList.innerHTML = '';
+
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+  const h2 = document.createElement('h2');
+  h2.innerHTML = book.name;
+  main.appendChild(h2);
   });
 }
 
-
-
-
-
+document.addEventListener('DOMContentLoaded', fetchBooks);
+  
 
